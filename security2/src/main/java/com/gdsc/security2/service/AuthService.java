75ca233f23ab573c15f281f0d440c5e566656afe
jiyoungmerong <jiyoungmerong@gmail.com>
2022-12-26 19:06:56 +0900

    허지영 과제 제출

---
+++ security2/src/main/java/com/gdsc/security2/service/AuthService.java
@@ -0,0 +1,27 @@
+package com.gdsc.security2.service;
+
+
+import com.gdsc.security2.entity.User;
+import com.gdsc.security2.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+@Service
+@RequiredArgsConstructor
+public class AuthService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException{
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return null;  //userdatails로 반환
+
+    }
+
+}
