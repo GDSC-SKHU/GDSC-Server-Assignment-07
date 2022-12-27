5baa6d9cc9dc432a41aeff67775169022662a1a8
LEEHYUNBOK <gusqhrzkq@naver.com>
2022-12-27 14:32:03 +0900

    이현복 과제제출

--- 이현복/src/test/java/com/example/security2/Security2ApplicationTests.java
+++ 이현복/src/test/java/com/example/security2/Security2ApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.gdsc07;
+package com.example.security2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Gdsc07ApplicationTests {
+class Security2ApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
